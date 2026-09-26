83592e02ad11c2581782484ea312014f1c4f9ae6
p92md87 <p92md87@gmail.com>
2026-09-26 12:57:05 +0200

    Actualiza referencia de caché a v018

--- app.js
+++ app.js
@@ -468,7 +468,7 @@
         await Promise.all(
           keys
             .filter(key=>key.startsWith("fugawi-control-") &&
-              key!=="fugawi-control-v017")
+              key!=="fugawi-control-v018")
             .map(key=>caches.delete(key))
         );
       } catch (_) {}
