5d00ba740b799b42d1bfd0e992aeb2a1ac67c1d7
p92md87 <p92md87@gmail.com>
2026-09-26 16:25:46 +0200

    Actualiza caché esperada a v024

--- app.js
+++ app.js
@@ -468,7 +468,7 @@
         await Promise.all(
           keys
             .filter(key=>key.startsWith("fugawi-control-") &&
-              key!=="fugawi-control-v023")
+              key!=="fugawi-control-v024")
             .map(key=>caches.delete(key))
         );
       } catch (_) {}
